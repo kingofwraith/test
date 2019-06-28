f2b2cf2b24ddfa055cfbef9cc8bec00b3faedf88
kingofwraith <909324641@qq.com>
2019-06-28 23:40:45 +0800

    add some notes

--- p_test.py
+++ p_test.py
@@ -1,10 +1,24 @@
+import time;
+import datetime;
+import os;
+
+
 print("=-=- syc learning python -=-=");
 
 while True:
-   t1=input("Please write something:(take 'exit'or'logout' to leave)\n>>> ");
-   if t1=="exit" or t1=="logout":
-       break;
-   print("::: "+t1);
+    t1=input("Please write something:(take 'exit'or'logout' to leave)\n>>> ");
+    if t1=="exit" or t1=="logout":
+        break;
+    print("::: "+t1);
 
 print("\n:::: [END]");
+
+
+while True:
+    t1= input("测试一行命令: \n>>>");
+    if t1=="exit" or t1=="logout":
+        break;
+    print(os.system(t1));
+print("\n:::: [END]");
+
 
