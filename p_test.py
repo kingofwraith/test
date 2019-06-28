ee1332aa9980c83e1355046a82e84133d1d41fb9
kingofwraith <909324641@qq.com>
2019-06-28 22:20:56 +0800

    add python test file

---
+++ p_test.py
@@ -0,0 +1,10 @@
+print("=-=- syc learning python -=-=");
+
+while True:
+   t1=input("Please write something:(take 'exit'or'logout' to leave)\n>>> ");
+   if t1=="exit" or t1=="logout":
+       break;
+   print("::: "+t1);
+
+print("\n:::: [END]");
+
