bd25353efb69df9e6c5570f5b279f99cdf22eb69
kingofwraith <909324641@qq.com>
2019-07-02 20:29:43 +0800

    write something

--- code/p_test.py
+++ code/p_test.py
@@ -2,8 +2,12 @@
 #-*-coding:utf-8-*-
 
 import time;
+import sys;
 import datetime;
 import os;
 
 print("=-=- syc learning python -=-=");
 
+for i in range(10):
+    time.sleep(5);
+    os.system("date>>testlog");
