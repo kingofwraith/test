fb06d38daeff87de1ddc9e4db290b23dd9f299a5
kingofwraith <909324641@qq.com>
2019-07-03 20:30:48 +0800

    change something

--- code/makePyFile.py
+++ code/makePyFile.py
@@ -2,27 +2,40 @@
 #-*-coding:utf-8-*-
 import sys,os
 
-logfile="work.log";
-fileName="default";
+logfile="makePyFile.log";
+fileName="defaultsyc";
+pathin=None;
 
 if len(sys.argv)>1:
     fileName=sys.argv[1];
+'''
 if len(sys.argv)>2:
-    logfile=sys.argv[2];
-
+    pathin=sys.argv[2];
+    pathin=pathin+"/";
+if len(sys.argv)>3:
+    logfile=sys.argv[3];
+'''
 
 #write logfile
 os.system("echo '=========='>>"+logfile);
 os.system("date>>"+logfile);
 log=open(logfile,"a");
-log.write("FROM::: makePyFile.py \r\nSTR:::: make file:"+fileName+".py \r\n");
+log.write("FROM::: makePyFile.py \nSTR:::: make file:"+fileName+".py \n");
 log.close();
 
 # the top words in "*.py" file
-toplist=["#!/usr/bin/env python\r\n","#-*-coding:utf-8-*-\r\n"];
+toplist=["#!/usr/bin/env python\n","#-*-coding:utf-8-*-\n","\n\n\n"];
+deflist=["def "+fileName+"():\n","    intmp=None;\n","\n\n","#"+fileName+"();\n"];
 
-myfile=open(fileName+".py","a");
+myfile=None;
+if pathin==None:
+    myfile=open(fileName+".py","a");
+else:
+    myfile=open(pathin+fileName+".py","a");
+
 myfile.writelines(toplist);
+myfile.writelines(deflist);
+
 myfile.close();
 
 os.system("chmod u+x "+fileName+".py");
