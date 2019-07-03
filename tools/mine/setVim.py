e8764e468c631eee3c9f1dedbbf68d7468b8a8fe
kingofwraith <909324641@qq.com>
2019-07-03 21:00:14 +0800

    add something

--- tools/mine/setVim.py
+++ tools/mine/setVim.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
+import os,sys;
 
+argv_setvim=sys.argv;
 
-
+# set vim in Centos 7
 def setVim():
-    intmp=None;
+    os.system("echo 'set number'>>/etc/vimrc");
+    os.system("echo 'set tabstop=4'>>/etc/vimrc");
+    os.system("echo 'set softtabstop=4'>>/etc/vimrc");
+    os.system("echo 'set shiftwidth=4'>>/etc/vimrc");
+    os.system("echo 'set expandtab'>>/etc/vimrc");
+    os.system("echo 'set list'>>/etc/vimrc");
 
 
-#setVim();
+setVim();
