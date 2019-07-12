f34d4911028ab2e9e0e640ca4f57686808cc487d
kingofwraith <909324641@qq.com>
2019-07-12 15:39:44 +0800

    python to github

--- tools/mine/sycGit.py
+++ tools/mine/sycGit.py
@@ -2,9 +2,31 @@
 #-*-coding:utf-8-*-
 import os,sys,pexpect;
 
+startword=['#','>>>','>','\$'];
 
+# use pexpect to work
 def sycGit():
-    intmp=None;
+    commd="git pull origin master";
+    # write work log
+    worklog=open("sycGit.log","w");
+    # keywords of ssh_key
+    key="_rsa";
+    child=pexpect.spawn(commd);
+    child.logfile=worklog;
+    
+    ret=child.expect([pexpect.TIMEOUT,pexpect.EOF,key]);
+    if ret==0:
+        print ":: ERROR :: TIMEOUT";
+    if ret==1:
+        print ":: ERROR :: EOF";
+    if ret==2:
+        #catch the keywords and  send ssh_key
+        child.sendline("sycsyc");
+        child.logfile=worklog;
+    worklog.close();
 
+# use os.system to work
+def sycGit2():
+    commd="git pull origin master";
 
-#sycGit();
+sycGit();
