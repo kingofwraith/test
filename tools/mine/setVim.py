46352b98914e7502781643ae33ead050e0f66a55
kingofwraith <909324641@qq.com>
2019-07-26 22:28:14 +0800

    add codes for <Vi>

--- tools/mine/setVim.py
+++ tools/mine/setVim.py
@@ -3,6 +3,7 @@
 import os,sys;
 
 argv_setvim=sys.argv;
+#print(argv_setvim[1]);
 
 # set vim in Centos 7
 def setVim():
@@ -13,5 +14,14 @@ def setVim():
     os.system("echo 'set expandtab'>>/etc/vimrc");
     os.system("echo 'set list'>>/etc/vimrc");
 
+def setVi():
+    os.system("echo 'set number'>>/etc/virc");
+    os.system("echo 'set tabstop=4'>>/etc/virc");
+    os.system("echo 'set softtabstop=4'>>/etc/virc");
+    os.system("echo 'set shiftwidth=4'>>/etc/virc");
+    os.system("echo 'set expandtab'>>/etc/virc");
+    os.system("echo 'set list'>>/etc/virc");
 
-setVim();
+#setVim();
+#setVi();
+
