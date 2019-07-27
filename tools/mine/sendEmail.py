137fae3cf3211802dcf57df4c5687da8570fbc75
kingofwraith <909324641@qq.com>
2019-07-27 12:43:08 +0800

    python for email

---
+++ tools/mine/sendEmail.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import smtplib;
+from email.mine.text import MINEText;
+from email.header import Header;
+
+
+def sendEmail():
+#    intmp=None;
+    sender='kingofwraith';
+    recesiver='909324641@qq.com';
+    subject='pythone email test';
+    
+
+
+#sendEmail();
