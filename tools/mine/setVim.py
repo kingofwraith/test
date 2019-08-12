d65dc5a6a38fd6d1a02cfec504782ab50516b0d6
kingofwraith <909324641@qq.com>
2019-08-12 20:59:03 +0800

    auto upload by python

--- tools/mine/setVim.py
+++ tools/mine/setVim.py
@@ -14,6 +14,10 @@ def setVim():
     os.system("echo 'set expandtab'>>/etc/vimrc");
     os.system("echo 'set list'>>/etc/vimrc");
 
+    os.system("echo 'set fileencodings=utf-8,ucs-bom,gb18030,gbk,gb2312,cp936'>>/etc/vimrc");
+    os.system("echo 'set termencoding=utf-8'>>/etc/vimrc");
+    os.system("echo 'set encoding=utf-8'>>/etc/vimrc");
+
 def setVi():
     os.system("echo 'set number'>>/etc/virc");
     os.system("echo 'set tabstop=4'>>/etc/virc");
@@ -22,6 +26,22 @@ def setVi():
     os.system("echo 'set expandtab'>>/etc/virc");
     os.system("echo 'set list'>>/etc/virc");
 
-#setVim();
-#setVi();
+    os.system("echo 'set fileencodings=utf-8,ucs-bom,gb18030,gbk,gb2312,cp936'>>/etc/virc");
+    os.system("echo 'set termencoding=utf-8'>>/etc/virc");
+    os.system("echo 'set encoding=utf-8'>>/etc/virc");
 
+def main():
+    tOption=None;
+    if len(sys.argv)>1:
+        tOption=sys.argv[1];
+    else:
+        print "No option here";
+
+    if tOption=='vim':
+        setVim();
+
+    if tOption=='vi':
+        setVi();
+
+if __name__=='__main__':
+    main();
