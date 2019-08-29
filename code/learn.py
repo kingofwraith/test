18d0ce5a7a1b1b07493b5ae5d7447af3f4e1bf55
kingofwraith <909324641@qq.com>
2019-08-29 21:18:09 +0800

    learn tmux

---
+++ code/learn.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+
+
+def learn():
+    intmp=None;
+
+
+def main():
+#learn 1
+    data = ['syc',1987,11.26,(13,25,303)];
+    name,year,day,address = data;
+    print('------learn 1------');
+    print(name);
+    print(year);
+    print(day);
+    print(address[1]);
+
+
+if __name__=='__main__':
+    main();
